09fc9910d2088e6d9e118b02fc55710f76d7ea05
Gourav Kumar <gouravkumarat@gmail.com>
2022-12-24 21:06:04 +0000

    Update 2 files

--- src/App.js
+++ src/App.js
@@ -5,66 +5,70 @@ const myChoice = {
   books: {
     javascript: [
       { name: "Eloquent JavaScript", rating: "4/5" },
-      { name: "You Don't Know JS", rating: "3.5/5" }
+      { name: "You Don't Know JS", rating: "3.5/5" },
     ],
 
     fiction: [
       {
         name: "Shiva Trilogy",
-        rating: "5/5"
+        rating: "5/5",
       },
       {
         name: "Harry Potter and the Sorcerer's Stone",
-        rating: "4.5/5"
-      }
+        rating: "4.5/5",
+      },
     ],
     business: [
       {
         name: "Never Split the Difference",
-        rating: "3.5/5"
+        rating: "3.5/5",
       },
       {
         name: "Loonshots",
-        rating: "5/5"
-      }
-    ]
+        rating: "5/5",
+      },
+    ],
   },
   Songs: {
     Rap: [
       { name: "Ram Ram by mc square", rating: "4.5/5" },
-      { name: "SQl By Sidhu musewala", rating: "4/5" }
+      { name: "SQl By Sidhu musewala", rating: "4/5" },
     ],
     Romantic: [
       { name: "Ram Ram by mc square", rating: "4.5/5" },
-      { name: "SQl By Sidhu musewala", rating: "4/5" }
+      { name: "SQl By Sidhu musewala", rating: "4/5" },
     ],
     old_songs: [
       { name: "Ram Ram by mc square", rating: "4.5/5" },
-      { name: "SQl By Sidhu musewala", rating: "4/5" }
-    ]
+      { name: "SQl By Sidhu musewala", rating: "4/5" },
+    ],
   },
   movies: {
     Hollywood: [
       { name: "Ram Ram by mc square", rating: "4.5/5" },
-      { name: "SQl By Sidhu musewala", rating: "4/5" }
+      { name: "SQl By Sidhu musewala", rating: "4/5" },
     ],
     Bollywood: [
       { name: "Ram Ram by mc square", rating: "4.5/5" },
-      { name: "SQl By Sidhu musewala", rating: "4/5" }
+      { name: "SQl By Sidhu musewala", rating: "4/5" },
     ],
     jollywood: [
       { name: "Ram Ram by mc square", rating: "4.5/5" },
-      { name: "SQl By Sidhu musewala", rating: "4/5" }
-    ]
-  }
+      { name: "SQl By Sidhu musewala", rating: "4/5" },
+    ],
+  },
 };
+
 export default function App() {
+  const [selectedbranch , setbranch] = userState("");
   const [selectedgenre, setgenre] = useState("books");
   function onClickHandler(choice) {
     setgenre(choice);
   }
 
-  function onSelectHandler() {}
+  function onSelectHandler(book) {
+    setbranch(book);
+  }
   return (
     <div className="App">
       <h1> 📚 myChoice </h1>
@@ -78,10 +82,11 @@ export default function App() {
         ))}
       </div>
       <hr />
+
       <div>
-        {Object.keys(selectedgenre).map((book) => (
+        {Object.keys(mychoice[selectedgenre]).map((book) => (
           <span key={book}>
-            <button onClick={() => onSelectHandler()}></button>
+            <button onClick={() => onSelectHandler(book)}>{book}</button>
           </span>
         ))}
       </div>
